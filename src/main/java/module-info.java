2425f6f995766d38e6f8753a92dd87e2221888fc
Manujaravishka <manujaravishka2004@gmail.com>
2025-09-04 13:38:36 +0530

    create

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lk.ijse.orm_final_course_work {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lk.ijse.orm_final_course_work to javafx.fxml;
+    exports lk.ijse.orm_final_course_work;
+}

b61a0cfe546f2c84ad9b3bc37851e2cec388bb56
Manujaravishka <manujaravishka2004@gmail.com>
2025-09-04 13:54:22 +0530

    StudentDto

--- src/main/java/lk/ijse/orm_final_course_work/dto/StudentDto.java
+++ src/main/java/lk/ijse/orm_final_course_work/dto/StudentDto.java
@@ -1,4 +1,18 @@
 package lk.ijse.orm_final_course_work.dto;
 
+import lk.ijse.orm_final_course_work.entity.Course;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class StudentDto {
+    private int studentID;
+    private String studentName;
+    private String studentEmail;
+    private String contactNO;
+    private String address;
+    private LocalDate dateOfBirth;
+    private LocalDate registrationDate;
+    private List<Course> courses;
+    private List<LessonDto> lessons;
 }

1ef18414473bdf88f4c718d619ea454f182997e5
Manujaravishka <manujaravishka2004@gmail.com>
2025-09-04 13:50:49 +0530

    LessonsDto

--- src/main/java/lk/ijse/orm_final_course_work/dto/LessonDto.java
+++ src/main/java/lk/ijse/orm_final_course_work/dto/LessonDto.java
@@ -1,4 +1,19 @@
 package lk.ijse.orm_final_course_work.dto;
 
+
+import lk.ijse.orm_final_course_work.entity.Course;
+import lk.ijse.orm_final_course_work.entity.Instructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class LessonDto {
+    private String lessonId;
+    private String lessonName;
+    private LocalDate lessonDate;
+    private LocalTime lessonStartTime;
+    private LocalTime lessonEndTime;
+    private Course course;
+    private Instructor instructor;
+
 }

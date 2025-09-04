cd61e74a0f4453a2b740cc6a709c71aa5c9faa0b
Manujaravishka <manujaravishka2004@gmail.com>
2025-09-04 13:54:37 +0530

    UserDto

--- src/main/java/lk/ijse/orm_final_course_work/dto/UserDto.java
+++ src/main/java/lk/ijse/orm_final_course_work/dto/UserDto.java
@@ -1,4 +1,11 @@
 package lk.ijse.orm_final_course_work.dto;
 
 public class UserDto {
+
+    private String userId;
+    private String username;
+    private String password;
+    private String role;
+    private boolean active;
+
 }

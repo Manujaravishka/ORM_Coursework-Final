5d97ae72ab20a256e6d480ade553e9ff77a1edb7
Manujaravishka <manujaravishka2004@gmail.com>
2025-09-10 23:15:22 +0530

    today

--- src/main/java/lk/ijse/orm_final_course_work/bo/custom/InstructorBO.java
+++ src/main/java/lk/ijse/orm_final_course_work/bo/custom/InstructorBO.java
@@ -1,4 +1,17 @@
 package lk.ijse.orm_final_course_work.bo.custom;
 
-public interface InstructorBO {
+import lk.ijse.orm_final_course_work.bo.SuperBO;
+import lk.ijse.orm_final_course_work.dto.InstructorDTO;
+
+import java.util.List;
+
+public interface InstructorBO extends SuperBO {
+
+    boolean saveInstructor(InstructorDTO dto);
+    boolean updateInstructor(InstructorDTO dto);
+    boolean deleteInstructor(String id);
+    InstructorDTO searchInstructor(String id);
+    List<InstructorDTO> getAllInstructors();
+    String generateNewId();
+
 }

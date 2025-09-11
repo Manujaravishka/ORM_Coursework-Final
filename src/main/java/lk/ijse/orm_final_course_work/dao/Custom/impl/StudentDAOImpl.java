1d3d6acb87da7e268d899d7850765103009230eb
Manujaravishka <manujaravishka2004@gmail.com>
2025-09-11 20:58:33 +0530

    Dao

--- src/main/java/lk/ijse/orm_final_course_work/dao/Custom/impl/StudentDAOImpl.java
+++
@@ -1,4 +0,0 @@
-package lk.ijse.orm_final_course_work.dao.Custom.impl;
-
-public class StudentDAOImpl {
-}
--- src/main/java/lk/ijse/orm_final_course_work/exception/UserAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.orm_final_course_work.exception;
-
-public class UserAlreadyExistException extends RuntimeException {
-    public UserAlreadyExistException(String message) {
-        super(message);
-    }
-}
